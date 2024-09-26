d0925d435b0c15ecfd5ad2ced911cd6a0c13edf6
ryandorrington <dorringtonryan@gmail.com>
2024-09-26 13:39:43 +0800

    use BEAM search

--- learning/gpt2/train_gpt2.py
+++ learning/gpt2/train_gpt2.py
@@ -2,7 +2,7 @@ import os
 from os import getenv
 import time
 
-from tinygrad import nn, Tensor, TinyJit, Device
+from tinygrad import nn, Tensor, TinyJit, Device, Context
 from tinygrad.nn.optim import AdamW
 from tinygrad import dtypes
 from tinygrad.helpers import getenv
@@ -239,18 +239,19 @@ for k, x in nn.state.get_state_dict(model).items(): x.to_(GPUS)
 optimizer = AdamW(nn.state.get_parameters(model), lr=3e-4, b2=0.95, weight_decay=0.1)
 
 def step():
-    t0 = time.time()
-    x, y = train_loader.next_batch()
+    with Context(BEAM=2):
+        t0 = time.time()
+        x, y = train_loader.next_batch()
 
-    optimizer.zero_grad()
-    logits = model(x)
-    loss = logits.reshape(-1, logits.shape[-1]).cross_entropy(y.flatten()).backward()
-    optimizer.step()
-    Device[Device.DEFAULT].synchronize()
-    t1 = time.time()
-    dt = (t1 - t0) * 1000
-    tokens_per_second = (train_loader.B * train_loader.T) / (t1 - t0)
-    return loss, dt, tokens_per_second
+        optimizer.zero_grad()
+        logits = model(x)
+        loss = logits.reshape(-1, logits.shape[-1]).cross_entropy(y.flatten()).backward()
+        optimizer.step()
+        Device[Device.DEFAULT].synchronize()
+        t1 = time.time()
+        dt = (t1 - t0) * 1000
+        tokens_per_second = (train_loader.B * train_loader.T) / (t1 - t0)
+        return loss, dt, tokens_per_second
 
 tiny_step = TinyJit(step)
 
